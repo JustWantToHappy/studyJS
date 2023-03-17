8d3d3e17b4039f35770d6eadb3377e9ee25a1ab2
啦啦啦啦啦 <1302755003@qq.com>
2023-03-17 15:08:22 +0800

    feat call-apply-bind函数模拟

---
+++ 手写题/call-bind-apply方法实现.js
@@ -0,0 +1,41 @@
+//call方法的实现
+Function.prototype._call = function (context,...args) {
+    context = typeof context !== "object" ? window : context;
+    //避免原有属性被覆盖
+    const key = Symbol();
+    context[key] = this;
+    const result = context[key](...args);
+    delete context[key];
+    return result;
+}
+//apply方法的实现
+Function.prototype._apply = function (context,args) {
+    context = typeof context !== "object" ? window : context;
+    //避免原有属性被覆盖
+    const key = Symbol();
+    context[key] = this;
+    const result = context[key](...args);
+    delete context[key];
+    return result;
+}
+
+// bind方法的实现
+Function.prototype._bind = function (target) {
+    let that = typeof window === "object" ? window : globalThis;
+    //如果给bind函数指定的this指向为null或者undefined，则将this指向改为全局对象
+    target = (typeof target) !== "object" ? that : target;
+    let fn = this;
+    let args1 = [].slice.call(arguments, 1);
+    const bindClone = function () {
+        let args2 = Array.prototype.slice.call(arguments);
+        fn.call(this instanceof bindClone?fn:target,args1.concat(args2));
+    }
+    let Bar = function () { }
+    Bar.prototype = bindClone.prototype;
+    bindClone.prototype = new Bar();
+    return bindClone;
+}
+function test(a,b) {
+    console.info(a, b,this.name);
+}
+test._bind({ name: "sb" },'a','b')();

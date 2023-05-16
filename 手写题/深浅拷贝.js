133652992ad318916ada615c76885aafd49cbeeb
啦啦啦啦啦 <1302755003@qq.com>
2023-05-16 22:30:31 +0800

    feat bfs实现深拷贝

--- 手写题/深浅拷贝.js
+++ 手写题/深浅拷贝.js
@@ -42,13 +42,10 @@ const dfs_deep_copy = function (obj) {
 /**
  * 预备知识:环状数据
  * const obj={
- *  name:"john",
- *  age:12,
  *  children:[]
- * };
- * obj.children.push(obj);
- * @desc 它的children属性应用了它自己，由于是递归复制，会一直追踪children属性，导致死循环。
- */
+ * }
+ * obj.children.push(obj)
+ * const newObj=deepClone(obj);//需要解决循环依赖问题
 
 /**
  * 深拷贝需要解决的问题:
@@ -68,7 +65,12 @@ const getType = (item) => {
  */
 //~index等价交换index!==-1
 
-const isObect = (target) => (typeof target === "object" || typeof target === "function") && target !== null;
+/**
+ * 
+ * @param {any} target 
+ * @param {Map} map 解决循环依赖导致的无限递归问题
+ * @returns 
+ */
 function DFSdeepClone(target, map = new WeakMap()) {
     if (map.get(target)) return target;
     //获取当前值的构造函数，获取到它的类型
@@ -76,24 +78,68 @@ function DFSdeepClone(target, map = new WeakMap()) {
     if (/^(RegExp|Date)$/i.test(constructor.name)) {
         return new constructor(target);
     }
-
-    if (isObect(target)) {
-        map.set(target.true);
+    let type = getType(target);
+    if (type === "Array" || type === "Object") {
+        map.set(target, true);
         const cloneTarget = Array.isArray(target) ? [] : {};
         for (let prop in target) {
             //遍历自身属性，不关心原型链上的属性
             if (target.hasOwnProperty(prop)) {
-                cloneTarget[prop] = deepClone(target[prop], map);
+                cloneTarget[prop] = DFSdeepClone(target[prop], map);
             }
         }
         return cloneTarget;
+    } else if (type === "Function") {
+        return eval("(" + target.toString() + ")");
     } else {
         return target;
     }
 }
 
-
 //深拷贝bfs完全版本
-
-
-
+function BFSDeepColone(target) {
+    let queue = [target];
+    let copyObj = {};
+    let copy_queue = [copyObj];
+    let map = new WeakMap();//用来解决循环依赖问题
+    while (queue.length > 0) {
+        const currentObj = queue.shift();
+        let copy = copy_queue.shift();
+        let type = getType(currentObj);
+        switch (type) {
+            case "Object" || "Array":
+                for (let prop in currentObj) {
+                    let value = currentObj[prop];
+                    let type = getType(value);
+                    if (type === "Object") {
+                        //如果触发循环依赖
+                        if (map.get(value)) {
+                            copy[prop] = value;
+                            map.set(copy, true);
+                        } else {
+                            copy[prop] = {};
+                            queue.push(value);
+                            copy_queue.push(copy[prop]);
+                        }
+                    } else if (type === "Array") {
+                        copy[prop] = [];
+                        queue.push(value);
+                        copy_queue.push(copy[prop]);
+                    } else if (type === "Function") {
+                        copy[prop] = eval("(" + value.toString() + ")");
+                    } else {
+                        copy[prop] = value;
+                    }
+                }
+                break;
+            case "Function":
+                copyObj = eval("(" + currentObj.toString() + ")");
+                break;
+            default:
+                copyObj = currentObj;
+                break;
+        }
+    }
+    return copyObj;
+}
+console.info(BFSDeepColone({name:"dfsdfs",age:{dsfsdf:2423}}))

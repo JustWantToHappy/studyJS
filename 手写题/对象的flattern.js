9345deaa161fdd910074e0fadca58a44110ce3ff
啦啦啦啦啦 <1302755003@qq.com>
2023-05-23 12:10:12 +0800

    fix README.md

--- 手写题/对象的flattern.js
+++ 手写题/对象的flattern.js
@@ -5,48 +5,55 @@
  */
 const flattern = (obj) => {
     let newObj = {};
+
+    const getType = (obj) => Object.prototype.toString.call(obj).slice(8, -1);
+    const isNumber = (num) => getType(num) === "Number";
+
     function handleTypes(path = [], obj, key) {
         let keyType = typeof key;
         let valueType = typeof obj[key];
-        let type = Object.prototype.toString.call(obj[key]);
         let value = obj[key];
-        if (keyType === "number"&&valueType==="number") {
+        let type = getType(obj[key]);
+        if (keyType === "number" && valueType === "number") {
             let newKey = path.join(".");
             newObj[newKey] = value;
             return true;
-        } else if (keyType === "string" &&type!=="[object Array]"&&type!=="[object Object]") {
+        } else if (keyType === "string" && type !== "Array" && type !== "Object") {
             let newKey = path.join(".");
             newObj[newKey] = value;
             return true;
         }
         return false;
     }
-    function flat(obj,path) {
-        let type = Object.prototype.toString.call(obj);
-        let flag1 = type === "[object Object]";
-        let flag2 = type === "[object Array]";
-        if (flag1||flag2) {
+
+    function flat(obj, path = []) {
+        let objType = getType(obj);
+        if (["Array", "Object"].includes(objType)) {
             for (let key in obj) {
                 if (!obj.hasOwnProperty(key)) {
                     continue;
                 }
-                if (flag2) {
-                    let top = path.pop();
-                    top += "[" + key + "]";
-                    path.push(top);
+                let type = getType(obj[key]);
+                let isnum = isNumber(key);
+                if (objType === "Array") {
+                    let arrPath = path.pop();
+                    arrPath += `[${key}]`;
+                    path.push(arrPath);
                 } else {
                     path.push(key);
                 }
-                !handleTypes(path, obj,key) && flat(obj[key], path);
+                !handleTypes(path, obj, key) && flat(obj[key], path);
                 path.pop();
             }
-        }  else {    
+        } else {
             return false;
         }
     }
-    flat(obj, []);
+    flat(obj);
     return newObj;
 }
 
-let myobj = { name: "hhh", age: 12, list: [1, [2, [3, 4]]], func() { }, info: {gender:"男",time:Date.now} };
+let myobj = { name: "hhh", age: 12, list: [1, [2, [3, 4]]], func() { }, info: { gender: "男", time: Date.now } };
 console.info(flattern(myobj));
+
+

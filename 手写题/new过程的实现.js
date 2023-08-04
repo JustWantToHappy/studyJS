94a16c90942e5dc9de5b425affc734cb22d827eb
啦啦啦啦啦 <1302755003@qq.com>
2023-08-04 22:14:48 +0800

    feat 打平数组的几种方式.js

--- 手写题/new过程的实现.js
+++ 手写题/new过程的实现.js
@@ -1,8 +1,8 @@
 //模拟new的实现
-function toNew(fn,...args) {
+function toNew(fn, ...args) {
     //返回一个对象，并将这个对象的原型指向fn的原型
     let fnPrototype = Object.create(fn.prototype);
-    const result = fn.apply(fnPrototype, ...args);
+    const result = fn.apply(fnPrototype, args);
     //如果fn的构造函数中返回了一个对象
     return typeof result === "obejct" ? result : fnPrototype;
 }
